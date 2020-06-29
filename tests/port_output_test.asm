85be4d0cf0b4062de0c797d08475e1ca1e3943b1
Sh4dewarrior <giosaata99@gmail.com>
2020-06-29 22:22:24 +0400

    Add port output test

---
+++ tests/port_output_test.asm
@@ -0,0 +1,7 @@
+@256			// 0100					//0100 # Select 256
+D = -A;			// 1000 0100 1000 1000	//8488 # Write 1111 1111 0000 0000 in D_Reg
+@0				// 0000					//0	   # Select DDR_Reg
+M = D;			// 1000 0000 1101 0000	//80d0 # Write D_Reg in DDR_Reg
+@1				// 0001					//0001 # Select Port_Reg
+M = -1;			// 1000 0011 0101 0000	//8350 # Write 1111 1111 1111 1111 in Port_Reg
+				//							   # Turn on first 8 LEDs
